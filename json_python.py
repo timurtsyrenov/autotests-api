b9976011bacb95e8cc5801218425879f9f03d58b
timurtsyrenov <timurmurmurik@mail.ru>
2025-09-20 20:26:41 +0700

    Знакомство с gRPC протоколом

---
+++ json_python.py
@@ -0,0 +1,24 @@
+import json
+
+json_data = '{"name": "Иван", "age": 30, "is_student": false}'
+parsed_data = json.loads(json_data)  # Преобразуем JSON-строку в Python-объект (dict)
+
+print(parsed_data["name"])  # Выведет: Иван
+
+
+data = {
+    "name": "Мария",
+    "age": 25,
+    "is_student": True
+}
+
+json_string = json.dumps(data, indent=4)  # Преобразуем Python-объект в JSON-строку
+print(json_string)
+
+
+with open("json_example.json", "r", encoding="utf-8") as file:
+    read_data = json.load(file)  # Загружаем JSON из файла
+    print(data)
+
+with open("json_example_w.json", "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)  # Сохраняем JSON в файл
