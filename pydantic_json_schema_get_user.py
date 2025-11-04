79cb1c5b7ab7774424eda2cbc0d4176f03ff7267
timurtsyrenov <timurmurmurik@mail.ru>
2025-11-04 19:17:22 +0700

    "Валидация JSON schema.Практикуемся в работе с JSON schema"

---
+++ pydantic_json_schema_get_user.py
@@ -0,0 +1,33 @@
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import get_private_users_client
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema, GetUserResponseSchema
+from tools.assertions.schema import validate_json_schema
+from tools.fakers import get_random_email
+
+# Инициализируем клиент PublicUsersClient
+public_users_client = get_public_users_client()
+
+# Инициализируем запрос на создание пользователя
+create_user_request = CreateUserRequestSchema(
+    email=get_random_email(), password="string", last_name="string", first_name="string", middle_name="string"
+)
+# Отправляем POST запрос на создание пользователя
+create_user_response = public_users_client.create_user(create_user_request)
+
+# Инициализируем пользовательские данные для аутентификации
+authentication_user = AuthenticationUserSchema(email=create_user_request.email, password=create_user_request.password)
+# Инициализируем клиент PrivateUsersClient
+private_users_client = get_private_users_client(authentication_user)
+
+# Отправляем GET запрос на получение данных пользователя
+get_user_response = private_users_client.get_user_api(create_user_response.user.id)
+
+# Получаем JSON-схему из Pydantic-модели ответа
+get_user_response_schema = GetUserResponseSchema.model_json_schema()
+
+# Проверяем, что JSON-ответ от API соответствует ожидаемой JSON-схеме
+validate_json_schema(instance=get_user_response.json(), schema=get_user_response_schema)
+
+print(get_user_response.json())
+print(get_user_response_schema)
