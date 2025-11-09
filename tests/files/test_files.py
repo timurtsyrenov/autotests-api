23f8e232839429be59e4c46ce32ef90df958d139
timurtsyrenov <timurmurmurik@mail.ru>
2025-11-09 21:51:36 +0700

    "Практикуемся в написании API автотестов. Часть 1.Практика написания негативных API автотестов для работы с файлами"

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -9,7 +9,7 @@ from fixtures.files import FileFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.files import assert_create_file_response, assert_get_file_response, \
     assert_create_file_with_empty_filename_response, assert_create_file_with_empty_directory_response, \
-    assert_file_not_found_response
+    assert_file_not_found_response, assert_get_file_with_incorrect_file_id_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -84,3 +84,17 @@ class TestFiles:
 
         # 6. Проверяем, что ответ соответствует схеме
         validate_json_schema(get_response.json(), get_response_data.model_json_schema())
+
+    def test_get_file_with_incorrect_file_id(self, files_client: FilesClient):
+        response = files_client.get_file_api("incorrect-file-id")
+        response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
+
+        print(response_data)
+
+        # Проверка, что код ответа соответствует ожиданиям (422 - Unprocessable Entity)
+        assert_status_code(response.status_code, HTTPStatus.UNPROCESSABLE_ENTITY)
+        # Проверка, что ответ API соответствует ожидаемой валидационной ошибке
+        assert_get_file_with_incorrect_file_id_response(response_data)
+
+        # Дополнительная проверка структуры JSON
+        validate_json_schema(response.json(), response_data.model_json_schema())
