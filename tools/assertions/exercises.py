0681ad3e16baadf51954379c3a8c9e2ff074c6b4
timurtsyrenov <timurmurmurik@mail.ru>
2025-11-10 23:24:23 +0700

    "Практикуемся в написании API автотестов. Часть 3.Практика написания API автотестов для работы с заданиями. Получение задания"

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import (CreateExerciseRequestSchema, CreateExerciseResponseSchema,
+                                                ExerciseSchema, GetExerciseResponseSchema)
 from tools.assertions.base import assert_equal
 
 
@@ -17,3 +18,34 @@ def assert_create_exercise_response(request: CreateExerciseRequestSchema, respon
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные задания соответствуют ожидаемым.
+
+    :param actual: Фактические данные задания.
+    :param expected: Ожидаемые данные задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(
+    get_exercise_response: GetExerciseResponseSchema, create_exercise_response: CreateExerciseResponseSchema
+):
+    """
+    Проверяет, что данные задания при создании и при запросе совпадают.
+
+    :param get_exercise_response: Ответ API при запросе задания.
+    :param create_exercise_response: Ответ API при создании задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_exercise(actual=get_exercise_response.exercise, expected=create_exercise_response.exercise)
