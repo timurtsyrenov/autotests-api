6814c7cd4e6c539a2043a2e867e5671703ee2489
timurtsyrenov <timurmurmurik@mail.ru>
2025-11-11 19:53:42 +0700

    "Практикуемся в написании API автотестов. Часть 3.Практика написания API автотестов для работы с заданиями. Удаление задания"

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,7 +1,9 @@
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_schema import (CreateExerciseRequestSchema, CreateExerciseResponseSchema,
                                                 ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema,
                                                 UpdateExerciseResponseSchema)
 from tools.assertions.base import assert_equal
+from tools.assertions.errors import assert_internal_error_response
 
 
 def assert_create_exercise_response(request: CreateExerciseRequestSchema, response: CreateExerciseResponseSchema):
@@ -66,3 +68,14 @@ def assert_update_exercise_response(request: UpdateExerciseRequestSchema, respon
     assert_equal(response.exercise.order_index, request.order_index, "order_index")
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+
+
+def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
+    """
+    Функция для проверки ошибки, если задание не найдено на сервере.
+
+    :param actual: Фактический ответ.
+    :raises AssertionError: Если фактический ответ не соответствует ошибке "Exercise not found"
+    """
+    expected = InternalErrorResponseSchema(details="Exercise not found")
+    assert_internal_error_response(actual, expected)
