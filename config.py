9d955009f7ce8208349058146e0ccf5643d798e0
timurtsyrenov <timurmurmurik@mail.ru>
2026-05-17 12:21:57 +0700

    "Запуск автотестов на GitHub Actions"

--- config.py
+++ config.py
@@ -19,6 +19,7 @@ class TestDataConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        extra='allow',  # Разрешаем дополнительные переменные
         env_file=".env",  # Указываем, из какого файла читать настройки
         env_file_encoding="utf-8",  # Указываем кодировку файла
         env_nested_delimiter=".",  # Указываем разделитель для вложенных переменных
